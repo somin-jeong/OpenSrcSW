6a571761e976e86fd0e875eaa4859cd995559d34
DESKTOP-BL0LQD5\정소민 <yjk70702002@gmail.com>
2021-04-08 14:44:14 +0900

    master branch - CalcSim2 to CalcSim

--- searcher.java
+++ searcher.java
@@ -37,7 +37,7 @@ public class searcher {
 		System.out.println(data);
 	}
 	
-	public static double[] CalcSim2(String args) throws IOException, ClassNotFoundException {
+	public static double[] CalcSim(String args) throws IOException, ClassNotFoundException {
 		KeywordExtractor ke = new KeywordExtractor();
 		KeywordList kl = ke.extractKeyword(data, true);
 		
@@ -158,7 +158,7 @@ public class searcher {
 		}
 		
 		
-		double[] id = CalcSim2(args);
+		double[] id = CalcSim(args);
 		
 		
 		
